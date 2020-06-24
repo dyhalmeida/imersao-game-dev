49fe29863f9d4003e95953244a1c82520285418c
Diego Almeida <dyhalmeida@gmail.com>
2020-06-24 11:50:38 -0300

    adiciona classe generica de personagem e criando a hispta

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-let personage;
-let heigthPersonage = 135;
-let widthPersonage = 110;
+let hipstaImage;
+let hipsta;
+const matrizSpriteHipsta = [
+    [0, 0],
+    [220, 0],
+    [440, 0],
+    [660, 0],
+    [0, 270],
+    [220, 270],
+    [440, 270],
+    [660, 270],
+    [0, 540],
+    [220, 540],
+    [440, 540],
+    [660, 540],
+    [0, 810],
+    [220, 810],
+    [440, 810],
+    [660, 810],
+]
+let soundJump;
 
 let scenario;
 let sound;
 
 function preload() {
     scenarioImage = loadImage('./assets/scenario/floresta.png');
-    personageImage = loadImage('./assets/personage/correndo.png');
-    sound = loadSound('/assets/sound/trilha_jogo.mp3');
+    hipstaImage = loadImage('./assets/personage/correndo.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     scenario = scenarioFactory(scenarioImage, 2);
-    personage = personageFactory(personageImage);
+    hipsta = new Hipsta(matrizSpriteHipsta, hipstaImage, 0, 110, 135, 220, 270);
     frameRate(40);
-    sound.loop();
+    gameSound.loop();
 }
 
 function draw() {
     scenario.show();
-    personage.show();
+    hipsta.show();
 }
 
 const scenarioFactory = (scenarioImage, velocity) => {
@@ -48,28 +65,50 @@ const scenarioFactory = (scenarioImage, velocity) => {
     }
 }
 
-const personageFactory = (personageImage) => {
+class Personage {
 
-    frames = [
-        [0, 0],
-        [220, 0],
-        [440, 0],
-        [660, 0],
-        [0, 270],
-        [220, 270],
-        [440, 270],
-        [660, 270],
-        [0, 540],
-        [220, 540],
-        [440, 540],
-        [660, 540],
-        [0, 810],
-        [220, 810],
-        [440, 810],
-        [660, 810],
-    ]
+    constructor(sprite, imagePersonage, positionX, widthPersonage, heightPersonage, widthSprite, heightSprite) {
+        this.sprite = sprite;
+        this.imagePersonage = imagePersonage;
+        this.widthPersonage = widthPersonage;
+        this.heightPersonage = heightPersonage;
+        this.positionX = positionX;
+        this.positionY = height - this.heightPersonage;
+        this.widthSprite = widthSprite;
+        this.heightSprite = heightSprite;
+        this.currentFrame = 0;
+    }
 
-    let currentFrame = 0;
+    show() {
+        image(
+            this.imagePersonage, 
+            this.positionX,
+            this.positionY, 
+            this.widthPersonage, 
+            this.heightPersonage, 
+            this.sprite[this.currentFrame][0],
+            this.sprite[this.currentFrame][1],
+            this.widthSprite, 
+            this.heightSprite
+        );
+
+        this.animate();
+    }
+
+    animate() {
+        this.currentFrame++;
+        if (this.currentFrame >= this.sprite.length - 1) this.currentFrame = 0;
+    }
+}
+
+class Hipsta extends Personage {
+    constructor(sprite, imagePersonage, positionX, widthPersonage, heightPersonage, widthSprite, heightSprite) {
+        super(sprite, imagePersonage, positionX, widthPersonage, heightPersonage, widthSprite, heightSprite);
+        this.gravity = 3;
+        this.jumpVelocity = 0;
+        this.positionYBase = height - this.heightPersonage;
+        this.positionY = this.positionYBase;
+    }
 
     const show = () => {
         image(
